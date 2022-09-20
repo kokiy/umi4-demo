7046955bb22766b679013d6a86edd8fba53c272c
koki <zghong@huanbaoyun.cloud>
2022-09-20 17:16:16 +0800

    test demo

---
+++ jest.config.ts
@@ -0,0 +1,24 @@
+import { Config, configUmiAlias, createConfig } from "umi/test";
+
+export default async () => {
+  return (await configUmiAlias({
+    ...createConfig({
+      target: "browser",
+      jsTransformer: "esbuild",
+      // config opts for esbuild , it will pass to esbuild directly
+      jsTransformerOpts: { jsx: "automatic" },
+    }),
+    setupFilesAfterEnv: ["<rootDir>/jest-setup.ts"],
+    collectCoverageFrom: [
+      "src/**/*.{ts,js,tsx,jsx}",
+      "!src/.umi/**",
+      "!src/.umi-test/**",
+      "!src/.umi-production/**",
+    ],
+    // if you require some es-module npm package, please uncomment below line and insert your package name
+    // transformIgnorePatterns: ['node_modules/(?!.*(lodash-es|your-es-pkg-name)/)']
+    transformIgnorePatterns: [
+      "/node_modules/(?!antd|@ant-design|rc-.+?|@babel/runtime|@umijs/renderer-react|@umijs/preset-umi|umi).+(js|jsx)$",
+    ],
+  })) as Config.InitialOptions;
+};
---
+++ src/pages/__tests__/index.test.jsx
@@ -0,0 +1,7 @@
+import { render } from "@testing-library/react";
+import Index from "../index";
+describe("test page", () => {
+  it("init", () => {
+    render(<Index />);
+  });
+});
